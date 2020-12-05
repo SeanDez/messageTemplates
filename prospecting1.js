bf139cc58c0bf081a6786fad2591792880d76ec6
Sean Dez <sd@seandezoysa.com>
2020-12-05 18:27:14 -0500

    add bit ly link variations

--- prospecting1.js
+++ prospecting1.js
@@ -1,18 +1,18 @@
 {
-Hi %name%, {I saw |I've {seen|read}} %companyName% {had|has} {a recent|an} opening for a web developer. Can you {pass|send|give|relay} my information to the {hiring {person|staff|manager}|recruiter|{staff{person|member| }|person} in charge}? My {website|portfolio site} {including|and|with linked|along with my} resume: https://bit.ly/39KShBj
+Hi %name%, {I saw |I've {seen|read}} %companyName% {had|has} {a recent|an} opening for a web developer. Can you {pass|send|give|relay} my information to the {hiring {person|staff|manager}|recruiter|{staff{person|member| }|person} in charge}? My {website|portfolio site} {including|and|with linked|along with my} resume: {https://bit.ly/39KShBj|https://bit.ly/33Nb3EF|https://bit.ly/2JEU8x1|https://bit.ly/2JSNyCY|https://bit.ly/39NcBCe|https://bit.ly/3mNeIcZ|https://bit.ly/37yD184|https://bit.ly/3oy8Tkd|https://bit.ly/3lLT9YX|https://bit.ly/37yD184}
 |
 Hi %name%, do you know who the {{recruiting|hiring} {person|manager}|recruiter|staff{person|member}} is for the web developer position recently {advertised|posted}? If you could {refer|send|pass} me to {him/her|the right individual} that would {help{ a lot| }|be great}, {thanks {so much| }|thank you}. 
 
-My info (resume and {website|portfolio site}): https://bit.ly/39KShBj
+My resume and {website|portfolio site}: {https://bit.ly/39KShBj|https://bit.ly/33Nb3EF|https://bit.ly/2JEU8x1|https://bit.ly/2JSNyCY|https://bit.ly/39NcBCe|https://bit.ly/3mNeIcZ|https://bit.ly/37yD184|https://bit.ly/3oy8Tkd|https://bit.ly/3lLT9YX|https://bit.ly/37yD184}
 |
 Hi %name%,
 
-I'm not {sure|certain|aware} who I should {talk to|be {emailing|contacting}}, maybe you could {help|point me in the right direction|assist|direct me}? %companyName% has posted {an open role|a position} for a web developer and I would like to {apply|send my resume|submit my info for consideration}. {Who is best to get in touch with|Who is the right person to talk to} regarding this {open| } {position|role|opportunity}?
+I'm not {sure|certain|aware} who I should {talk to|be {emailing|contacting}}, maybe you could {help|point me in the right direction|assist|direct me}? %companyName% has posted {an open role|a position} for a web developer and I would like to {apply|send my resume|submit my info for consideration}. {Who is best to {speak|get in touch} with|Who is the {best|correct|right} person to {speak|talk} to} {about|regarding} this {open|} {position|role|opportunity}?
 
 {Thank you|Thanks},
-My {website|portfolio site} {including|and|with linked|along with my} resume: https://bit.ly/39KShBj
+My {website|portfolio site} {including|and|with linked|along with my} resume: {https://bit.ly/39KShBj|https://bit.ly/33Nb3EF|https://bit.ly/2JEU8x1|https://bit.ly/2JSNyCY|https://bit.ly/39NcBCe|https://bit.ly/3mNeIcZ|https://bit.ly/37yD184|https://bit.ly/3oy8Tkd|https://bit.ly/3lLT9YX|https://bit.ly/37yD184}
 |
 Hi %name%,
 
-I was {happy|glad|enthusiastic} to see that %companyName% is {seeking|looking for|hiring|recruiting} {new| } web developers. Can you {refer|send|pass} me to the {hiring {person|staff|manager}|recruiter|{staff{person|member| }|right {person|individual|team member|person in charge}}}? My resume and {website|portfolio{ | site}}: https://bit.ly/39KShBj
+I was {happy|glad|enthusiastic} to see that %companyName% is {seeking|looking for|hiring|recruiting} {new| } web developers. Can you {refer|send|pass} me to the {hiring {person|staff|manager}|recruiter|{staff{person|member| }|right {person|individual|team member|person in charge}}}? My resume and {website|portfolio{ | site}}: {https://bit.ly/39KShBj|https://bit.ly/33Nb3EF|https://bit.ly/2JEU8x1|https://bit.ly/2JSNyCY|https://bit.ly/39NcBCe|https://bit.ly/3mNeIcZ|https://bit.ly/37yD184|https://bit.ly/3oy8Tkd|https://bit.ly/3lLT9YX|https://bit.ly/37yD184}
 }
