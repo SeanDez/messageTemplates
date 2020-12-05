760b2fe1331ff9cb2f0a3ac4943fd4d93a6d8490
Sean Dez <sd@seandezoysa.com>
2020-12-05 18:06:17 -0500

    initial commit

---
+++ prospecting1.js
@@ -0,0 +1,18 @@
+{
+Hi %name%, {I saw |I've {seen|read}} %companyName% {had|has} {a recent|an} opening for a web developer. Can you {pass|send|give|relay} my information to the {hiring {person|staff|manager}|recruiter|{staff{person|member| }|person} in charge}? My {website|portfolio site} {including|and|with linked|along with my} resume: https://bit.ly/39KShBj
+|
+Hi %name%, do you know who the {{recruiting|hiring} {person|manager}|recruiter|staff{person|member}} is for the web developer position recently {advertised|posted}? If you could {refer|send|pass} me to {him/her|the right individual} that would {help{ a lot| }|be great}, {thanks {so much| }|thank you}. 
+
+My info (resume and {website|portfolio site}): https://bit.ly/39KShBj
+|
+Hi %name%,
+
+I'm not {sure|certain|aware} who I should {talk to|be {emailing|contacting}}, maybe you could {help|point me in the right direction|assist|direct me}? %companyName% has posted {an open role|a position} for a web developer and I would like to {apply|send my resume|submit my info for consideration}. {Who is best to get in touch with|Who is the right person to talk to} regarding this {open| } {position|role|opportunity}?
+
+{Thank you|Thanks},
+My {website|portfolio site} {including|and|with linked|along with my} resume: https://bit.ly/39KShBj
+|
+Hi %name%,
+
+I was {happy|glad|enthusiastic} to see that %companyName% is {seeking|looking for|hiring|recruiting} {new| } web developers. Can you {refer|send|pass} me to the {hiring {person|staff|manager}|recruiter|{staff{person|member| }|person}? My resume and {website|portfolio{ | site}}: https://bit.ly/39KShBj
+}
