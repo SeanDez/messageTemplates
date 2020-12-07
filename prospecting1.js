d74c6cb72b034c242ada8962583ecbe2da80a468
Sean Dez <sd@seandezoysa.com>
2020-12-07 07:11:13 -0500

    change name to firstName

--- prospecting1.js
+++ prospecting1.js
@@ -1,17 +1,17 @@
 {
-Hi %name%, I saw that %companyName% has {an opening| a new position} for a web developer. Can you {pass|send|relay} my information to the {hiring {person|manager}|recruiter}? My {website|portfolio site} {including|and|with linked|along with my} resume: 
+Hi %firstName%, I saw that %companyName% has {an opening| a new position} for a web developer. Can you {pass|send|relay} my information to the {hiring {person|manager}|recruiter}? My {website|portfolio site} {including|and|with linked|along with my} resume: 
 
 http://seandz.com
 https://docs.google.com/document/d/1B04GszEveekuiB2JWZwvgZTWFpWx2J5xlsTFNHbkuFQ/edit?usp=sharing
 |
-Hi %name%, do you know who the {hiring {person|manager}|recruiter} is for the web developer position recently {advertised|posted}? If you could {refer|send|pass} me to {him/her|the right individual} that would {help|be great}, {thanks so much|thank you}. 
+Hi %firstName%, do you know who the {hiring {person|manager}|recruiter} is for the web developer position recently {advertised|posted}? If you could {refer|send|pass} me to {him/her|the right individual} that would {help|be great}, {thanks so much|thank you}. 
 
 My resume and {website|portfolio site}: 
 
 http://seandz.com
 https://docs.google.com/document/d/1B04GszEveekuiB2JWZwvgZTWFpWx2J5xlsTFNHbkuFQ/edit?usp=sharing
 |
-Hi %name%,
+Hi %firstName%,
 
 I'm not {sure|certain} who I should {talk to|be {emailing|contacting}}, maybe you could {help|point me in the right direction|direct me}? %companyName% posted {an open role|a position} for a web developer and I would like to {apply|send my resume|submit my info for consideration}. {Who is best to {speak|get in touch} with|Who is the {best|right} person to {speak|talk} to} {about|regarding} this {position|opening}?
 
@@ -22,7 +22,7 @@ My {website|portfolio site} {including|and|with linked|along with my} resume:
 http://seandz.com
 https://docs.google.com/document/d/1B04GszEveekuiB2JWZwvgZTWFpWx2J5xlsTFNHbkuFQ/edit?usp=sharing
 |
-Hi %name%,
+Hi %firstName%,
 
 I was {happy|glad} to see that %companyName% is {seeking|looking for|hiring|recruiting} web developers. Can you {refer|send|pass} me to the {hiring {person|manager}|recruiter}? My resume and {website|portfolio site}: 
 
